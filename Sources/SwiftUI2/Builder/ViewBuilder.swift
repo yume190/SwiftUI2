da9817b51e86ee6e93b71cdc5a573e2469aa091f
Yume <yume190@gmail.com>
2020-12-07 10:49:56 +0800

    ViewBuilder implement buildBlock 2~10

--- Sources/SwiftUI2/Builder/ViewBuilder.swift
+++ Sources/SwiftUI2/Builder/ViewBuilder.swift
@@ -7,56 +7,86 @@
 
 import UIKit
 
-//public static func buildBlock() -> EmptyView
-//public static func buildIf<Content>(_ content: Content?) -> Content? where Content : View
-//public static func buildEither<TrueContent, FalseContent>(first: TrueContent) -> _ConditionalContent<TrueContent, FalseContent> where TrueContent : View, FalseContent : View
-//public static func buildEither<TrueContent, FalseContent>(second: FalseContent) -> _ConditionalContent<TrueContent, FalseContent> where TrueContent : View, FalseContent : View
-
-//public static func buildLimitedAvailability<Content>(_ content: Content) -> AnyView where Content : View
-
-//public static func buildBlock<C0, C1>(_ c0: C0, _ c1: C1) -> TupleView<(C0, C1)> where C0 : View, C1 : View
-//build 2 ~ 10
 @_functionBuilder
 public enum ViewBuilder {
+    /// public static func buildBlock() -> EmptyView
     public static func buildBlock() -> UIView {
         return UIView(frame: .zero)
     }
     
-//    public static func buildBlock<Content: View>(_ content: Content) -> Content.Body {
-//        return content.body
-//    }
     public static func buildBlock<Content: View>(_ content: Content) -> Content {
         return content
     }
     
+    /// build 2
     public static func buildBlock<C0: View, C1: View>(_ c0: C0, _ c1: C1) -> [UIView] {
         return [c0.ui, c1.ui].compactMap(rSelf)
     }
     
+    /// build 3
     public static func buildBlock<C0: View, C1: View, C2: View>(_ c0: C0, _ c1: C1, _ c2: C2) -> [UIView] {
         return [c0.ui, c1.ui, c2.ui].compactMap(rSelf)
     }
     
-    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View>(
-        _ c0: C0,
-        _ c1: C1,
-        _ c2: C2,
-        _ c3: C3,
-        _ c4: C4,
-        _ c5: C5,
-        _ c6: C6,
-        _ c7: C7
+    /// build 4
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3
     ) -> [UIView] {
         return [
-            c0.ui,
-            c1.ui,
-            c2.ui,
-            c3.ui,
-            c4.ui,
-            c5.ui,
-            c6.ui,
-            c7.ui
-            ].compactMap(rSelf)
+            c0.ui, c1.ui, c2.ui, c3.ui
+        ].compactMap(rSelf)
+    }
+    
+    /// build 5
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4
+    ) -> [UIView] {
+        return [
+            c0.ui, c1.ui, c2.ui, c3.ui, c4.ui
+        ].compactMap(rSelf)
+    }
+    
+    /// build 6
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5
+    ) -> [UIView] {
+        return [
+            c0.ui, c1.ui, c2.ui, c3.ui, c4.ui,
+            c5.ui
+        ].compactMap(rSelf)
+    }
+    
+    /// build 7
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6
+    ) -> [UIView] {
+        return [
+            c0.ui, c1.ui, c2.ui, c3.ui, c4.ui,
+            c5.ui, c6.ui
+        ].compactMap(rSelf)
+    }
+    
+    /// build 8
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7
+    ) -> [UIView] {
+        return [
+            c0.ui, c1.ui, c2.ui, c3.ui, c4.ui,
+            c5.ui, c6.ui, c7.ui
+        ].compactMap(rSelf)
+    }
+    
+    /// build 9
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8
+    ) -> [UIView] {
+        return [
+            c0.ui, c1.ui, c2.ui, c3.ui, c4.ui,
+            c5.ui, c6.ui, c7.ui, c8.ui
+        ].compactMap(rSelf)
+    }
+    
+    /// build 10
+    public static func buildBlock<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View, C9: View>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8, _ c9: C9
+    ) -> [UIView] {
+        return [
+            c0.ui, c1.ui, c2.ui, c3.ui, c4.ui,
+            c5.ui, c6.ui, c7.ui, c8.ui, c9.ui
+        ].compactMap(rSelf)
     }
     
     private static func rSelf(view: UIView?) -> UIView? {
@@ -67,10 +97,13 @@ public enum ViewBuilder {
 //        return UIStackView(arrangedSubviews: items.compactMap { $0 })
 //    }
     
+    //public static func buildIf<Content>(_ content: Content?) -> Content? where Content : View
 //    public static func buildIf(_ value: UIView?) -> UIView? {
 //        return value
 //    }
-//
+
+    //public static func buildEither<TrueContent, FalseContent>(first: TrueContent) -> _ConditionalContent<TrueContent, FalseContent> where TrueContent : View, FalseContent : View
+    //public static func buildEither<TrueContent, FalseContent>(second: FalseContent) -> _ConditionalContent<TrueContent, FalseContent> where TrueContent : View, FalseContent : View
 //    public static func buildEither(first value: UIView) -> UIView {
 //        return value
 //    }
@@ -78,4 +111,6 @@ public enum ViewBuilder {
 //    public static func buildEither(second value: UIView) -> UIView {
 //        return value
 //    }
+    
+    //public static func buildLimitedAvailability<Content>(_ content: Content) -> AnyView where Content : View
 }
