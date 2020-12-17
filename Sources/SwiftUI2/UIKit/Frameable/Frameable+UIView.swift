ddcb5379c81481641f0b386be8b027b7592911dd
Yume <yume190@gmail.com>
2020-12-17 14:40:30 +0800

    UIView add frame

---
+++ Sources/SwiftUI2/UIKit/Frameable/Frameable+UIView.swift
@@ -0,0 +1,22 @@
+//
+//  Frameable+UIView.swift
+//  
+//
+//  Created by Yume on 2020/12/17.
+//
+
+import class UIKit.UIView
+import CoreGraphics
+
+extension UIView {
+    public func frame(width: CGFloat? = nil, height: CGFloat? = nil, alignment: Alignment = .center) -> Self {
+        if let _width = width {
+            self.widthAnchor.constraint(equalToConstant: _width).isActive = true
+        }
+        
+        if let _height = height {
+            self.heightAnchor.constraint(equalToConstant: _height).isActive = true
+        }
+        return self
+    }
+}
