ac6d2a8e05ce5479b4e2b28047510b060a7cffbc
Yume <yume190@gmail.com>
2020-12-21 10:40:53 +0800

    add ForEach

---
+++ Sources/SwiftUI2/View/Origin/ForEach.swift
@@ -0,0 +1,31 @@
+//
+//  ForEach.swift
+//  
+//
+//  Created by Yume on 2020/12/21.
+//
+
+import UIKit
+
+public struct ForEach<Data: RandomAccessCollection, Content: View>: View {
+    public let data: Data
+    
+    public let content: (Data.Element) -> Content
+    
+    public var body: Never {
+        fatalError()
+    }
+    
+    public init(_ data: Data, @ViewBuilder content: @escaping (Data.Element) -> Content) {
+        self.data = data
+        self.content = content
+    }
+}
+
+extension ForEach: Container {
+    var _views: [UIView] {
+        return self.data
+            .map(self.content)
+            .flatMap(\._views)
+    }
+}
