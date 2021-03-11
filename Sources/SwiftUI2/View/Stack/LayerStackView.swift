ef3498363fea4b687ef15106442e90c9baf04954
Yume <yume190@gmail.com>
2021-03-11 16:08:28 +0800

    fix layer stack background color

--- Sources/SwiftUI2/View/Stack/LayerStackView.swift
+++ Sources/SwiftUI2/View/Stack/LayerStackView.swift
@@ -11,4 +11,17 @@ final class LayerStackView: UIStackView {
     final override class var layerClass: AnyClass {
         return CALayer.self
     }
+    
+    override var backgroundColor: UIColor? {
+        get {
+            guard let color = self.layer.backgroundColor else {
+                return nil
+            }
+            return UIColor.init(cgColor: color)
+        }
+
+        set {
+            self.layer.backgroundColor = newValue?.cgColor
+        }
+    }
 }
