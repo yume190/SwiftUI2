fccc2d60f475ca4e7d729e22f8eb98dda46d24e4
Yume <yume190@gmail.com>
2021-12-07 11:52:55 +0800

    disable builder

--- Sources/SwiftUI2/Builder/Builder.swift
+++ Sources/SwiftUI2/Builder/Builder.swift
@@ -1,39 +1,39 @@
+////
+////  File.swift
+////
+////
+////  Created by Yume on 2020/12/7.
+////
 //
-//  Builder.swift
+//import UIKit
 //
+//public protocol Builder: AnyObject {
+//    associatedtype Target: Builder
+//    init()
 //
-//  Created by Yume on 2020/12/7.
+//    @discardableResult
+//    @inlinable
+//    static func build(_ builder: (Target) -> ()) -> Target
 //
-
-import UIKit
-
-public protocol Builder: AnyObject {
-    associatedtype Target: Builder
-    init()
-    
-    @discardableResult
-    @inlinable
-    static func build(_ builder: (Target) -> ()) -> Target
-    
-    @discardableResult
-    @inlinable
-    func build(_ builder: (Target) -> ()) -> Target
-}
-
-public extension Builder {
-    @discardableResult
-    @inlinable
-    static func build(_ builder: (Self) -> ()) -> Self {
-        return Self().build(builder)
-    }
-    
-    @discardableResult
-    @inlinable
-    func build(_ builder: (Self) -> ()) -> Self {
-        builder(self)
-        return self
-    }
-}
-
-extension UIView: Builder {}
-extension UIBarItem: Builder {}
+//    @discardableResult
+//    @inlinable
+//    func build(_ builder: (Target) -> ()) -> Target
+//}
+// 
+//public extension Builder {
+//    @discardableResult
+//    @inlinable
+//    static func build(_ builder: (Self) -> ()) -> Self {
+//        return Self().build(builder)
+//    }
+//
+//    @discardableResult
+//    @inlinable
+//    func build(_ builder: (Self) -> ()) -> Self {
+//        builder(self)
+//        return self
+//    }
+//}
+//
+//extension UIView: Builder {}
+//extension UIBarItem: Builder {}
